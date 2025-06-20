98c69a4098b560fd2e9ab51013df442ac1768ddc
서주희 <maywngml@naver.com>
2025-06-20 16:29:49 +0900

    feat: 인터뷰 목록 세션 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Main, Introduction } from '@/components/home';
+import { Main, Introduction, InterviewList } from '@/components/home';
 
 export default function Home() {
   return (
     <main>
       <Main />
       <Introduction />
+      <InterviewList />
     </main>
   );
 }
---
+++ src/components/home/InterviewList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import InterviewCard from './InterviewCard';
+import { interviews } from '@/constants/interview';
+
+const InterviewList = () => {
+  return (
+    <section className='flex flex-col pt-8 pb-14 px-12 xl:justify-center'>
+      <h4 className='mt-6 font-bold text-[28px] leading-[1.5]'>
+        먼저 합류한 동료들의 이야기
+      </h4>
+      <div className='grid grid-cols-3 gap-6 mt-6'>
+        {interviews.map((interview) => (
+          <InterviewCard
+            key={`interview-card-${interview.interviewer}`}
+            interview={interview}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default InterviewList;
