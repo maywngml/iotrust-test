fe01df326be6ef286e659e701fabcd02b4473172
서주희 <maywngml@naver.com>
2025-06-20 16:50:02 +0900

    style: 조직 문화 목록, 인터뷰 목록 영역 스타일 수정

--- src/components/home/CultureList.tsx
+++ src/components/home/CultureList.tsx
@@ -4,17 +4,21 @@ import { cultures } from '@/constants/culture';
 
 const CultureList = () => {
   return (
-    <section className='flex flex-col py-14 px-12 xl:justify-center'>
-      <h4 className='mt-6 font-bold text-[28px] leading-[1.5]'>
-        아이오트러스트는 이렇게 일해요
-      </h4>
-      <div className='grid grid-cols-3 gap-6 mt-6'>
-        {cultures.map((culture) => (
-          <CultureCard
-            key={`culture-card-${culture.title}`}
-            culture={culture}
-          />
-        ))}
+    <section>
+      <div className='flex py-14 px-12 xl:justify-center'>
+        <div className='xl:w-[1200px]'>
+          <h4 className='mt-6 font-bold text-[28px] leading-[1.5]'>
+            아이오트러스트는 이렇게 일해요
+          </h4>
+          <div className='grid grid-cols-3 gap-6 mt-6'>
+            {cultures.map((culture) => (
+              <CultureCard
+                key={`culture-card-${culture.title}`}
+                culture={culture}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/home/InterviewList.tsx
+++ src/components/home/InterviewList.tsx
@@ -4,17 +4,19 @@ import { interviews } from '@/constants/interview';
 
 const InterviewList = () => {
   return (
-    <section className='flex flex-col pt-8 pb-14 px-12 xl:justify-center'>
-      <h4 className='mt-6 font-bold text-[28px] leading-[1.5]'>
-        먼저 합류한 동료들의 이야기
-      </h4>
-      <div className='grid grid-cols-3 gap-6 mt-6'>
-        {interviews.map((interview) => (
-          <InterviewCard
-            key={`interview-card-${interview.interviewer}`}
-            interview={interview}
-          />
-        ))}
+    <section className='flex pt-8 pb-14 px-12 xl:justify-center'>
+      <div className='xl:w-[1200px]'>
+        <h4 className='mt-6 font-bold text-[28px] leading-[1.5]'>
+          먼저 합류한 동료들의 이야기
+        </h4>
+        <div className='grid grid-cols-3 gap-6 mt-6'>
+          {interviews.map((interview) => (
+            <InterviewCard
+              key={`interview-card-${interview.interviewer}`}
+              interview={interview}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
